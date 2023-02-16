3285929ac3cfef0d691515f4659b8af1eff031d4
akhilbhaje01 <akhilbhaje01@gmail.com>
2023-02-16 11:52:03 +0530

    assignment 1st variable assign
    assignment 1st variable assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,26 @@
+console.log("********1********");
+var myFullName;
+console.log(myFullName);
+
+myFullName = "AKHIL BHAJE";
+console.log(myFullName);
+
+var myFullName = "AKHIL RATNAKAR BHAJE";
+console.log(myFullName);
+
+console.log("********2********");
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+console.log("********3********");
+
+var firstName = "FIRST NAME:AKHIL,";
+var lastName = "LAST NAME:BHAJE,";
+var age = " AGE:28,";
+var address = "ADDRESS:NAGPUR,";
+var pincode = "PINCODE:440030,";
+var gender = "GENDER:MALE,";
+var isMarried = "ISMARRIED:NO,";
+console.log(firstName,lastName,age,address,pincode,gender,isMarried);
+
