52576043db9ec88a2bd8d763095dca695d34da89
akhilbhaje01 <akhilbhaje01@gmail.com>
2023-02-20 13:53:34 +0530

    Create 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,15 @@
+var myName = "Virat Kohli";
+var fatherName = "Yogesh Kohli";
+var age = 31;
+var weight = 82.5;
+var isMarried = true;
+var isLazy = false;
+var typeOfMyName = typeof myName;
+console.log("typeof variable myName: ",typeOfMyName);
+console.log("typeof variable fatherName:",typeof fatherName);
+console.log("Typeof variable age:",typeof age);
+console.log("typeof variable isMarried:",typeof isMarried);
+
+var graduation;
+console.log("value of graduation variable is: ",graduation);
+console.log("typeof graduation is:");
