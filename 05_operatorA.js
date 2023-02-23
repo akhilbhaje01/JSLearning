6bbe5ea8b1257452d26f230c6f7ae072e42f16f5
akhilbhaje01 <akhilbhaje01@gmail.com>
2023-02-23 06:59:12 +0530

    Assignment on js
    Assignment on js

---
+++ 05_operatorA.js
@@ -0,0 +1,35 @@
+console.log("------------------------------step1.1--------------------------");
+function squareOfWorldLength(arg1){
+
+console.log(`The Given String is: ${arg1}`); 
+console.log(`The word length is: ${arg1.length}`);
+console.log(`The word length square is: ${arg1.length**2}`);
+
+}
+squareOfWorldLength("JavaScript");
+console.log("-------------------------------step1.2---------------------");
+squareOfWorldLength("Google Chrome");
+console.log("-------------------------------step1.3---------------------");
+squareOfWorldLength("Developer Smart");
+
+console.log("--------------------------------step2.1-----------------------");
+
+function developer(){
+
+    var str = "I am Angular developer";
+    console.log(`The string length is: ${str.length}`);
+    console.log(`The divide by total no. of words is:${str.length/4}`);
+    console.log("----------------------------step2.2-----------------------");
+    console.log(`The multiplication by total word is:${str.length*4}`);
+}
+developer()
+    
+
+
+
+ 
+
+
+
+ 
+ 
